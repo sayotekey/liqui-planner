85cdc61b84c301174d723e58f32bc43062253043
SayoTeKey <sayo.briefs@gmail.com>
2025-11-15 23:08:37 +0100

    Add Gesamtbilanz class with initial structure and HTML template

---
+++ src/js/classes/Gesamtbilanz.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/*
+    <!-- Gesamtbilanz -->
+    <aside id="gesamtbilanz">
+      <h1>Gesamtbilanz</h1>
+      <div class="gesamtbilanz-zeile einnahmen">
+        <span>Einnahmen:</span><span>0,00€</span>
+      </div>
+      <div class="gesamtbilanz-zeile ausgaben">
+        <span>Ausgaben:</span><span>0,00€</span>
+      </div>
+      <div class="gesamtbilanz-zeile bilanz">
+        <span>Bilanz:</span><span class="positiv">0,00€</span>
+      </div>
+    </aside>
+
+*/
+class Gesamtbilanz {}
