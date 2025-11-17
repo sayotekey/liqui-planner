82844adb6e09fc155d96763c3113dc32e0062695
SayoTeKey <sayo.briefs@gmail.com>
2025-11-17 17:32:11 +0100

    Implement Gesamtbilanz class with methods for updating and displaying financial summaries

--- src/js/classes/Gesamtbilanz.js
+++ src/js/classes/Gesamtbilanz.js
@@ -16,4 +16,112 @@
     </aside>
 
 */
-class Gesamtbilanz {}
+class Gesamtbilanz {
+  constructor() {
+    this._einnahmen = 0;
+    this._ausgaben = 0;
+    this._bilanz = 0;
+    this._html = this._htmlGenerieren();
+    this._anzeigen();
+  }
+
+  aktualisieren(eintraege) {
+    this._einnahmen = 0;
+    this._ausgaben = 0;
+    this._bilanz = 0;
+    // let neuegesamtbilanz = new Map();
+    // neuegesamtbilanz.set("einnahmen", 0);
+    // neuegesamtbilanz.set("ausgaben", 0);
+    // neuegesamtbilanz.set("bilanz", 0);
+
+    eintraege.forEach((eintrag) => {
+      switch (eintrag.typ()) {
+        case "einnahme":
+          this._einnahmen = this._einnahmen + eintrag.betrag();
+          this._bilanz = this._bilanz + eintrag.betrag();
+          // neuegesamtbilanz.set("einnahmen", neuegesamtbilanz.get("einnahmen") + eintrag.betrag());
+          // neuegesamtbilanz.set("bilanz", neuegesamtbilanz.get("bilanz") + eintrag.betrag());
+          break;
+        case "ausgabe":
+          this._ausgaben = this._ausgaben + eintrag.betrag();
+          this._bilanz = this._bilanz - eintrag.betrag();
+          // neuegesamtbilanz.set("ausgaben",neuegesamtbilanz.get("ausgaben") + eintrag.betrag());
+          // neuegesamtbilanz.set("bilanz",neuegesamtbilanz.get("bilanz") - eintrag.betrag());
+
+          // neuegesamtbilanz.ausgaben + eintrag.betrag();
+          // neuegesamtbilanz.bilanz - eintrag.betrag();
+          break;
+        default:
+          console.log(`Der Typ "${eintrag.typ()}" ist nicht bekannt.`);
+          break;
+      }
+    });
+
+    this._html = this._htmlGenerieren();
+    this._anzeigen();
+  }
+
+  // anhand der aktuellen gesamtbilanz die gesamtbilanz neu generieren
+  _htmlGenerieren() {
+    let gesamtbilanz = document.createElement("aside");
+    gesamtbilanz.setAttribute("id", "gesamtbilanz");
+
+    let ueberschrift = document.createElement("h1");
+    ueberschrift.textContent = "Gesamtbilanz";
+    gesamtbilanz.insertAdjacentElement("afterbegin", ueberschrift);
+
+    let einnahmenZeile = document.createElement("div");
+    einnahmenZeile.setAttribute("class", "gesamtbilanz-zeile einnahmen");
+    let einnahmenTitel = document.createElement("span");
+    einnahmenTitel.textContent = "Einnahmen";
+    einnahmenZeile.insertAdjacentElement("afterbegin", einnahmenTitel);
+    let einnahmenBetrag = document.createElement("span");
+    einnahmenBetrag.textContent = `${(this._einnahmen / 100)
+      .toFixed(2)
+      .replace(".", ",")}€`;
+    einnahmenZeile.insertAdjacentElement("beforeend", einnahmenBetrag);
+    gesamtbilanz.insertAdjacentElement("beforeend", einnahmenZeile);
+
+    let ausgabenZeile = document.createElement("div");
+    ausgabenZeile.setAttribute("class", "gesamtbilanz-zeile ausgaben");
+    let ausgabenTitel = document.createElement("span");
+    ausgabenTitel.textContent = "Ausgaben";
+    ausgabenZeile.insertAdjacentElement("afterbegin", ausgabenTitel);
+    let ausgabenBetrag = document.createElement("span");
+    ausgabenBetrag.textContent = `${(this._ausgaben / 100)
+      .toFixed(2)
+      .replace(".", ",")}€`;
+    ausgabenZeile.insertAdjacentElement("beforeend", ausgabenBetrag);
+    gesamtbilanz.insertAdjacentElement("beforeend", ausgabenZeile);
+
+    let bilanzZeile = document.createElement("div");
+    bilanzZeile.setAttribute("class", "gesamtbilanz-zeile bilanz");
+    let bilanzTitel = document.createElement("span");
+    bilanzTitel.textContent = "Bilanz";
+    bilanzZeile.insertAdjacentElement("afterbegin", bilanzTitel);
+    let bilanzBetrag = document.createElement("span");
+
+    this._bilanz >= 0
+      ? bilanzBetrag.setAttribute("class", "positiv")
+      : bilanzBetrag.setAttribute("class", "negativ");
+
+    bilanzBetrag.textContent = `${(this._bilanz / 100)
+      .toFixed(2)
+      .replace(".", ",")}€`;
+    bilanzZeile.insertAdjacentElement("beforeend", bilanzBetrag);
+    gesamtbilanz.insertAdjacentElement("beforeend", bilanzZeile);
+
+    return gesamtbilanz;
+  }
+
+  _anzeigen() {
+    // prüfen, ob bereits gesamtbilanz angezeigt wird ( als ArrowFunction geschrieben)
+    let gesamtbilanz = document.querySelector("#gesamtbilanz");
+    if (gesamtbilanz != null) {
+      gesamtbilanz.remove();
+    }
+    document
+      .querySelector("body")
+      .insertAdjacentElement("beforeend", this._html);
+  }
+}
--- src/js/classes/Haushaltsbuch.js
+++ src/js/classes/Haushaltsbuch.js
@@ -31,8 +31,7 @@ class Haushaltsbuch {
     // Methodenaufrufe anpassen
     this._eintraegeSortieren();
     this._eintraegeAnzeigen();
-    this._gesamtbilanzErstellen();
-    this._gesamtbilanzAnzeigen();
+    this._gesamtbilanz.aktualisieren(this._eintraege);
   }
 
   eintragEntfernen(timestamp) {
@@ -46,8 +45,7 @@ class Haushaltsbuch {
     this._eintraege.splice(startIndex, 1);
 
     this._eintraegeAnzeigen();
-    this._gesamtbilanzErstellen();
-    this._gesamtbilanzAnzeigen();
+    this._gesamtbilanz.aktualisieren(this._eintraege);
   }
 
   _eintraegeSortieren() {
@@ -85,115 +83,5 @@ class Haushaltsbuch {
     } else {
       console.error("Element '.monatsliste' not found.");
     }
-  }
-
-  // gesamtbilanz erstellen
-  _gesamtbilanzErstellen() {
-    let neuegesamtbilanz = new Map();
-    neuegesamtbilanz.set("einnahmen", 0);
-    neuegesamtbilanz.set("ausgaben", 0);
-    neuegesamtbilanz.set("bilanz", 0);
-
-    // als ArrowFunction geschrieben
-    this._eintraege.forEach((eintrag) => {
-      switch (eintrag.typ()) {
-        case "einnahme":
-          neuegesamtbilanz.set(
-            "einnahmen",
-            neuegesamtbilanz.get("einnahmen") + eintrag.betrag()
-          );
-          neuegesamtbilanz.set(
-            "bilanz",
-            neuegesamtbilanz.get("bilanz") + eintrag.betrag()
-          );
-          break;
-        case "ausgabe":
-          neuegesamtbilanz.set(
-            "ausgaben",
-            neuegesamtbilanz.get("ausgaben") + eintrag.betrag()
-          );
-          neuegesamtbilanz.set(
-            "bilanz",
-            neuegesamtbilanz.get("bilanz") - eintrag.betrag()
-          );
-
-          neuegesamtbilanz.ausgaben += eintrag.betrag();
-          neuegesamtbilanz.bilanz -= eintrag.betrag();
-          break;
-        default:
-          console.log(`Der Typ "${eintrag.typ()}" ist nicht bekannt.`);
-          break;
-      }
-    });
-    this.gesamtbilanz = neuegesamtbilanz;
-  }
-
-  // anhand der aktuellen gesamtbilanz die gesamtbilanz neu generieren
-  _htmlGesamtbilanzGenerieren() {
-    let gesamtbilanz = document.createElement("aside");
-    gesamtbilanz.setAttribute("id", "gesamtbilanz");
-
-    let ueberschrift = document.createElement("h1");
-    ueberschrift.textContent = "gesamtbilanz";
-    gesamtbilanz.insertAdjacentElement("afterbegin", ueberschrift);
-
-    let einnahmenZeile = document.createElement("div");
-    einnahmenZeile.setAttribute("class", "gesamtbilanz-zeile einnahmen");
-    let einnahmenTitel = document.createElement("span");
-    einnahmenTitel.textContent = "Einnahmen";
-    einnahmenZeile.insertAdjacentElement("afterbegin", einnahmenTitel);
-    let einnahmenBetrag = document.createElement("span");
-    einnahmenBetrag.textContent = `${(this.gesamtbilanz.get("einnahmen") / 100)
-      .toFixed(2)
-      .replace(".", ",")}€`;
-    einnahmenZeile.insertAdjacentElement("beforeend", einnahmenBetrag);
-    gesamtbilanz.insertAdjacentElement("beforeend", einnahmenZeile);
-
-    let ausgabenZeile = document.createElement("div");
-    ausgabenZeile.setAttribute("class", "gesamtbilanz-zeile ausgaben");
-    let ausgabenTitel = document.createElement("span");
-    ausgabenTitel.textContent = "Ausgaben";
-    ausgabenZeile.insertAdjacentElement("afterbegin", ausgabenTitel);
-    let ausgabenBetrag = document.createElement("span");
-    ausgabenBetrag.textContent = `${(this.gesamtbilanz.get("ausgaben") / 100)
-      .toFixed(2)
-      .replace(".", ",")}€`;
-    ausgabenZeile.insertAdjacentElement("beforeend", ausgabenBetrag);
-    gesamtbilanz.insertAdjacentElement("beforeend", ausgabenZeile);
-
-    let bilanzZeile = document.createElement("div");
-    bilanzZeile.setAttribute("class", "gesamtbilanz-zeile bilanz");
-    let bilanzTitel = document.createElement("span");
-    bilanzTitel.textContent = "Bilanz";
-    bilanzZeile.insertAdjacentElement("afterbegin", bilanzTitel);
-    let bilanzBetrag = document.createElement("span");
-
-    this.gesamtbilanz.get("bilanz") >= 0
-      ? bilanzBetrag.setAttribute("class", "positiv")
-      : bilanzBetrag.setAttribute("class", "negativ");
-    // jetzt als ternaerer Operator geschrieben
-    // if (this.gesamtbilanz.get("bilanz") >= 0) {
-    //     bilanzBetrag.setAttribute("class", "positiv");
-    // } else if (this.gesamtbilanz.get("bilanz") < 0) {
-    //     bilanzBetrag.setAttribute("class", "negativ");
-    // }
-    bilanzBetrag.textContent = `${(this.gesamtbilanz.get("bilanz") / 100)
-      .toFixed(2)
-      .replace(".", ",")}€`;
-    bilanzZeile.insertAdjacentElement("beforeend", bilanzBetrag);
-    gesamtbilanz.insertAdjacentElement("beforeend", bilanzZeile);
-
-    return gesamtbilanz;
-  }
-
-  _gesamtbilanzAnzeigen() {
-    // prüfen, ob bereits gesamtbilanz angezeigt wird ( als ArrowFunction geschrieben)
-    document.querySelectorAll("#gesamtbilanz").forEach((gesamtbilanz) =>
-      // ggf. gesamtbilanz entfernen
-      gesamtbilanz.remove()
-    );
-    document
-      .querySelector("body")
-      .insertAdjacentElement("beforeend", this._htmlGesamtbilanzGenerieren());
   }
 }
